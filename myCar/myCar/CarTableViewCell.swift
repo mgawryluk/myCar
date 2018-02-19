a15b4624245edea8b478ddcef0d4408dfedee555
Michał <michal.gawryluk@icloud.com>
2018-02-19 22:31:03 +0100

    Create CarTableViewCell and NewCarViewController

---
+++ myCar/myCar/CarTableViewCell.swift
@@ -0,0 +1,24 @@
+//
+//  CarTableViewCell.swift
+//  myCar
+//
+//  Created by Michał on 19/02/2018.
+//  Copyright © 2018 Michał Gawryluk. All rights reserved.
+//
+
+import UIKit
+
+class CarTableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
