894055e64bac47f78330b8eb97d29d23d5300290
Michał <michal.gawryluk@icloud.com>
2018-07-01 21:57:25 +0200

    Set a UIPickerView in FilterCostViewController
    Created a picker view to pick a year for costs filtering

--- myCar/myCar/Costs/FilterCostViewController.swift
+++ myCar/myCar/Costs/FilterCostViewController.swift
@@ -9,21 +9,26 @@
 import UIKit
 import Firebase
 
-class FilterCostViewController: UIViewController, UITextFieldDelegate {
+class FilterCostViewController: UIViewController, UITextFieldDelegate, UIPickerViewDelegate, UIPickerViewDataSource {
+    
+    
     
     var refCosts: DatabaseReference!
     var currentUser: String?
     var car: Car?
     var costs: Cost?
+    let pickerData = ["2010", "2011", "2012", "2013", "2014", "2015", "2016", "2017", "2018"]
     
     let filterDateTextField = UITextField()
-    let filterDatePickerView = UIDatePicker()
-
+   // let filterDatePickerView = UIDatePicker()
+    let filterDateView = UIPickerView()
     
     
     override func viewDidLoad() {
         super.viewDidLoad()
         self.view.backgroundColor = UIColor.white
+        filterDateView.dataSource = self
+        filterDateView.delegate = self
         
         refCosts = Database.database().reference().child("Users/\(currentUser!)/cars/\((car?.identifier)!)/Costs")
         
@@ -35,13 +40,13 @@ class FilterCostViewController: UIViewController, UITextFieldDelegate {
         filterDateTextField.rightAnchor.constraint(equalTo: view.rightAnchor, constant: -50).isActive = true
         filterDateTextField.heightAnchor.constraint(equalToConstant: 30).isActive = true
         
-        filterDatePickerView.datePickerMode = .date
-        filterDatePickerView.addTarget(self, action: #selector(self.datePickerValueChanged(datePicker:)), for: .valueChanged)
+//        filterDatePickerView.datePickerMode = .date
+//        filterDatePickerView.addTarget(self, action: #selector(self.datePickerValueChanged(datePicker:)), for: .valueChanged)
         
         filterDateTextField.textAlignment = .left
         filterDateTextField.borderStyle = .roundedRect
         filterDateTextField.placeholder = "Pick year"
-        filterDateTextField.inputView = filterDatePickerView
+        filterDateTextField.inputView = filterDateView
         filterDateTextField.font = UIFont.systemFont(ofSize: 17)
      
         
@@ -53,13 +58,33 @@ class FilterCostViewController: UIViewController, UITextFieldDelegate {
         // Dispose of any resources that can be recreated.
     }
     
+    func numberOfComponents(in pickerView: UIPickerView) -> Int {
+        return 1
+    }
     
-    @objc func datePickerValueChanged(datePicker: UIDatePicker) {
-        let dateFormatter = DateFormatter()
-        dateFormatter.dateStyle = .short
-        dateFormatter.timeStyle = .none
-        
-        filterDateTextField.text = dateFormatter.string(from: datePicker.date)
-        
+    func pickerView(_ pickerView: UIPickerView, numberOfRowsInComponent component: Int) -> Int {
+        return pickerData.count
+    }
+    
+    func pickerView(_ pickerView: UIPickerView, titleForRow row: Int, forComponent component: Int) -> String? {
+        return pickerData[row]
+    }
+    
+    func pickerView(_ pickerView: UIPickerView, didSelectRow row: Int, inComponent component: Int) {
+        self.filterDateTextField.text = pickerData[row]
+        self.view.endEditing(true)
+
+    
+    
+//    @objc func datePickerValueChanged(datePicker: UIPickerView) {
+//
+//        var yearPicker : [String] {
+//            var years = [String]()
+//            for i in (2000..<2018).reversed() {
+//                years.append("\(i)")
+//            }
+//            return years
+//        }
+//    }
     }
 }
